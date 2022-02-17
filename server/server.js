a67a62273d93e56bfeb51ff325832b50a56f45b5
Devesh Srivastava <devesh.srivastava@manyagroup.com>
2022-02-17 12:59:38 +0530

    packages added for deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
 import cors from "cors";
+import path from "path";
 // import router
 import authRouter from "./Router/authRouter.js";
 import notesRouter from "./Router/notesRouter.js";
@@ -19,7 +20,10 @@ app.use(cors());
 app.use("/api", authRouter);
 
 app.use("/api", notesRouter);
-
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../build"));
+});
 // server config
 app.listen(PORT, () => {
 	console.log(`server is running at ${PORT}`);
