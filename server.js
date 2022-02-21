302505c9dbe48b8f78115b602974cca0e13cef2a
Devesh Srivastava <devesh.srivastava@manyagroup.com>
2022-02-22 00:30:06 +0530

    commit to heroku

--- server.js
+++ server.js
@@ -28,12 +28,8 @@ app.use("/api", authRouter);
 
 app.use("/api", notesRouter);
 //serving the build folder
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
-}
+
+app.use(express.static("client/build"));
 
 // server config
 
