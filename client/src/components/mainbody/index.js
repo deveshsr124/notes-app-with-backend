46d7431992ff8472b4da826c15f165f8cecf747c
Devesh Srivastava <devesh.srivastava@manyagroup.com>
2022-03-02 19:12:55 +0530

    frontend authentication

--- client/src/components/mainbody/index.js
+++ client/src/components/mainbody/index.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 import Header from "../header";
 import { useNotesContextValue } from "../../context/NotesContext";
 import NotesCreate from "../NotesCreate";
 import Note from "../Note";
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, Skeleton } from "@mui/material";
 import axios from "axios";
 const Home = () => {
 	const [{ notes }, dispatch] = useNotesContextValue();
 	const [searchterm, setSearchTerm] = useState("");
+	const [isloading, setIsLoading] = useState(false);
 	const searchFilter = (val) => {
 		if (searchterm === "") {
 			return val;
@@ -19,21 +21,25 @@ const Home = () => {
 		getNotes();
 	}, []);
 	const getNotes = async () => {
+		setIsLoading(true);
 		const { data } = await axios.get("/api/all-notes", {
 			withCredentials: true,
 		});
 		try {
-			data.notes !== []
-				? dispatch({
-						type: "GET_ALL_NOTES",
-						payload: data.notes,
-				  })
-				: console.log("no notes found ");
+			setIsLoading(false);
+			if (data.notes !== []) {
+				dispatch({
+					type: "GET_ALL_NOTES",
+					payload: data.notes,
+				});
+			}
 		} catch (err) {
 			console.log(err);
 		}
 	};
-	console.log("notes", notes);
+	if (!localStorage.getItem("token")) {
+		return <Navigate to="/login" />;
+	}
 	return (
 		<div>
 			<Header searchTerm={searchterm} setSearchTerm={setSearchTerm} />
@@ -42,23 +48,35 @@ const Home = () => {
 			>
 				<NotesCreate />
 			</div>
-			<Box marginLeft="5%" marginTop="5%" marginRight="5%">
-				<Grid
-					container
-					spacing={{ xs: 2, md: 3 }}
-					columns={{ xs: 4, sm: 12, md: 12 }}
+
+			{isloading ? (
+				<Box
+					marginTop="5%"
+					display="flex"
+					alignItems="center"
+					justifyContent="center"
 				>
-					{notes
-						.filter((val) => searchFilter(val))
-						.map((item) => {
-							return (
-								<Grid item xs={12} sm={4} md={3} key={item._id}>
-									<Note note={item} dispatch={dispatch} id={item._id} />
-								</Grid>
-							);
-						})}
-				</Grid>
-			</Box>
+					<Skeleton variant="rectangular" width={210} height={118} />
+				</Box>
+			) : (
+				<Box marginLeft="5%" marginTop="5%" marginRight="5%">
+					<Grid
+						container
+						spacing={{ xs: 2, md: 3 }}
+						columns={{ xs: 4, sm: 12, md: 12 }}
+					>
+						{notes
+							.filter((val) => searchFilter(val))
+							.map((item) => {
+								return (
+									<Grid item xs={12} sm={4} md={3} key={item._id}>
+										<Note note={item} dispatch={dispatch} id={item._id} />
+									</Grid>
+								);
+							})}
+					</Grid>
+				</Box>
+			)}
 		</div>
 	);
 };
